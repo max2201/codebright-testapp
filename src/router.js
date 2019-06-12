60d4e2b99476d2e3a279a5032fd0dfbd56d2456e
max <max22012201@gmail.com>
2019-06-13 02:41:20 +0700

    -

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 import Comment from './views/Comment.vue';
-import Test from './views/Test.vue';
 
 Vue.use(Router);
 
@@ -25,11 +24,6 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About,
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: Test,
     },
   ],
   scrollBehavior(to) {
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,56 +6,55 @@
 
     <div class="commentsFlexWrap">
 
-<!--      <div class="commentsColumn">-->
-
-<!--        <div class="commentBlock">-->
-<!--          <h2 class="commentTitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>-->
-<!--          <p class="commentBody">-->
-<!--            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem-->
-<!--            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer-->
-<!--            took a galley of type and scrambled it to make a type specimen book.-->
-<!--          </p>-->
-<!--          <router-link class="readButton" to="/comment">Read more</router-link>-->
-<!--        </div>-->
-
-<!--        <div class="commentBlock">-->
-<!--          <h2 class="commentTitle">Where does it come from?</h2>-->
-<!--          <p class="commentBody">-->
-<!--            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots-->
-<!--            in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard-->
-<!--            McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more-->
-<!--            obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the-->
-<!--            word in classical literature, discovered the undoubtable source.-->
-<!--          </p>-->
-<!--          <router-link class="readButton" to="/comment">Read more</router-link>-->
-<!--        </div>-->
-
-<!--        <div class="commentBlock">-->
-<!--          <h2 class="commentTitle">If you are going to use a passage</h2>-->
-<!--          <p class="commentBody">-->
-<!--            If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't-->
-<!--            anything embarrassing hidden in the middle of text.-->
-<!--          </p>-->
-<!--          <router-link class="readButton" to="/comment">Read more</router-link>-->
-<!--        </div>-->
-
-<!--      </div>-->
       <div class="commentsColumn">
 
-        <div class="commentBlock" v-for="(post) in filterChetPosts(posts)">
+        <div class="commentBlock" v-for="(post) in filterEvenPosts(posts)">
           <h2 class="commentTitle">{{post.id}}{{post.title}}</h2>
           <p class="commentBody">{{post.body}}</p>
           <router-link class="readButton" to="/comment">Read more</router-link>
         </div>
+
+<!--                <div class="commentBlock">-->
+<!--                  <h2 class="commentTitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>-->
+<!--                  <p class="commentBody">-->
+<!--                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem-->
+<!--                    Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer-->
+<!--                    took a galley of type and scrambled it to make a type specimen book.-->
+<!--                  </p>-->
+<!--                  <router-link class="readButton" to="/comment">Read more</router-link>-->
+<!--                </div>-->
+
+<!--                <div class="commentBlock">-->
+<!--                  <h2 class="commentTitle">Where does it come from?</h2>-->
+<!--                  <p class="commentBody">-->
+<!--                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots-->
+<!--                    in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard-->
+<!--                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more-->
+<!--                    obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the-->
+<!--                    word in classical literature, discovered the undoubtable source.-->
+<!--                  </p>-->
+<!--                  <router-link class="readButton" to="/comment">Read more</router-link>-->
+<!--                </div>-->
+
+<!--                <div class="commentBlock">-->
+<!--                  <h2 class="commentTitle">If you are going to use a passage</h2>-->
+<!--                  <p class="commentBody">-->
+<!--                    If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't-->
+<!--                    anything embarrassing hidden in the middle of text.-->
+<!--                  </p>-->
+<!--                  <router-link class="readButton" to="/comment">Read more</router-link>-->
+<!--                </div>-->
 
       </div>
 
       <div class="commentsColumn">
-        <div class="commentBlock" v-for="(post) in filterNeChetPosts(posts)">
+
+        <div class="commentBlock" v-for="(post) in filterOddPosts(posts)">
           <h2 class="commentTitle">{{post.id}}{{post.title}}</h2>
           <p class="commentBody">{{post.body}}</p>
           <router-link class="readButton" to="/comment">Read more</router-link>
         </div>
+
 <!--        <div class="commentBlock">-->
 <!--          <h2 class="commentTitle">Why do we use it?</h2>-->
 <!--          <p class="commentBody">-->
@@ -90,47 +89,41 @@
 
     </div>
 
-
-<!--    <h3 class="bottomTitle">Lorem Ipsum is text of the typesetting industry</h3>-->
-
-<!--    <router-link class="homeButton" to="/">Back to homepage</router-link>-->
-
     <BackToHome/>
   </div>
 </template>
 
 <script>
-  import Vuex from 'vuex';
-  import store from '../store';
-
-
+import Vuex from 'vuex';
 import BackToHome from '@/components/BackToHome';
 
 export default {
   name: 'about',
+
   components: { BackToHome },
+
   computed:
     Vuex.mapState(['posts']),
-  store,
-  created() {
-    this.$store.dispatch('loadData'); // dispatch loading
-  },
+    created() {
+      this.$store.dispatch('loadData'); // dispatch loading
+    },
+
   methods: {
-    filterChetPosts: function (posts) {
-      return posts.filter(function (poster) {
-        if (posts.indexOf(poster) % 2 === 0)
-        return poster;
+    filterEvenPosts(posts) {
+      return posts.filter(function (post) {
+        if (posts.indexOf(post) % 2 === 0) {
+          return post;
+        }
       });
     },
-    filterNeChetPosts: function (posts) {
-      return posts.filter(function (poster) {
-        if (posts.indexOf(poster) % 2 === 1)
-          return poster;
+    filterOddPosts(posts) {
+      return posts.filter(function (post) {
+        if (posts.indexOf(post) % 2 === 1) {
+          return post;
+        }
       });
     },
   },
-
-
 };
 </script>
 
@@ -195,32 +188,4 @@ export default {
     text-decoration: none;
   }
 
-  .bottomTitle {
-    margin-top: 57px;
-    margin-bottom: 19px;
-    font-size: 24px;
-    opacity: 0.5;
-    font-family: 'sofia_prosemibold', sans-serif;
-    color: #111518;
-    line-height: 72px;
-    text-align: center;
-  }
-
-  .homeButton {
-    display: block;
-    width: 212px;
-    margin: 0 auto 100px;
-    border: solid 1px #60e3a1;
-    border-radius: 3px;
-    font-family: 'sofia_proregular', sans-serif;
-    color: #1d1e25;
-    font-size: 16px;
-    line-height: 53px;
-    text-align: center;
-    text-decoration: none;
-  }
-  .homeButton::before{
-    content:url('../assets/Vector_Smart_Object1.png');
-    margin-right: 15px;
-  }
 </style>
